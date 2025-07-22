0f18f281b6ebe1d5747422046008adda3469dfb8
Coronado <retrollaso@gmail.com>
2025-07-22 14:36:56 -0500

    deploy v7

--- src/components/interview/InterviewLayout.tsx
+++ src/components/interview/InterviewLayout.tsx
@@ -268,7 +268,7 @@ export default function InterviewLayout({
 
     const fetchExistingRoom = async () => {
       try {
-        const res = await fetch(`https://apigateway-b8exa0bnakh6bvhx.canadacentral-01.azurewebsites.net/services/be/stream-service/room/${interviewId}`, {
+        const res = await fetch(`https://codemirrorstream-b6b9evcfaqe3c3cv.canadacentral-01.azurewebsites.net/services/be/stream-service/room/${interviewId}`, {
           signal: controller.signal,
           headers: { Authorization: `Bearer ${token}` },
         });
